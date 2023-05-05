72f532f3208345bd8799fe2e5f7900b1895c77d1
LanseylowT <50694634+LanseylowT@users.noreply.github.com>
2023-05-05 12:32:49 +0800

    First Commit naken hehe

---
+++ src/Main.java
@@ -0,0 +1,79 @@
+import io.github.cdimascio.dotenv.Dotenv;
+import listeners.EventListeners;
+import net.dv8tion.jda.api.OnlineStatus;
+import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+import net.dv8tion.jda.api.sharding.DefaultShardManagerBuilder;
+import net.dv8tion.jda.api.sharding.ShardManager;
+import net.dv8tion.jda.api.utils.ChunkingFilter;
+import net.dv8tion.jda.api.utils.MemberCachePolicy;
+import net.dv8tion.jda.api.utils.cache.CacheFlag;
+
+import javax.security.auth.login.LoginException;
+
+public class Main {
+    private final ShardManager shardManager;
+    private final Dotenv config;
+
+    // Creating the constructor
+    /**
+    * Loads Environment Variables and builds the Bot Shard Manager
+    * @throws LoginException occurs when bot token is invalid
+    */
+    public Main() throws LoginException {
+        // Load all the Environment Variables in the project
+        config = Dotenv.configure().load();
+        // Setting up the Token for the Discord Bot
+        String token = config.get("TOKEN");
+
+        // Setting up the Shard Manager
+        DefaultShardManagerBuilder builder = DefaultShardManagerBuilder.createDefault(token);
+
+        // Enabling the necessary Intents for the bot
+        builder.enableIntents(GatewayIntent.MESSAGE_CONTENT);
+        builder.enableIntents(GatewayIntent.DIRECT_MESSAGES);
+        builder.enableIntents(GatewayIntent.GUILD_MESSAGES);
+
+        builder.enableIntents(GatewayIntent.GUILD_MESSAGE_TYPING);
+        builder.enableIntents(GatewayIntent.GUILD_PRESENCES);
+        builder.enableIntents(GatewayIntent.GUILD_MEMBERS);
+
+        // Enabling Caches
+        builder.setMemberCachePolicy(MemberCachePolicy.ALL);
+        builder.setChunkingFilter(ChunkingFilter.ALL);
+        builder.enableCache(CacheFlag.ONLINE_STATUS);
+
+        // Disabling Caches
+//        builder.disableCache(CacheFlag.VOICE_STATE, CacheFlag.EMOJI, CacheFlag.STICKER, CacheFlag.SCHEDULED_EVENTS);
+
+        // Set the status for the bot
+        builder.setStatus(OnlineStatus.ONLINE);
+
+        // Setting up the activity for the bot
+        builder.setActivity(Activity.listening("Your Mom's Moans"));
+
+        // Build the actual Shard Manager for the Bot
+        shardManager = builder.build();
+
+        // Register Listeners
+        shardManager.addEventListener(new EventListeners());
+    }
+
+    // Setting up the Config getters
+    public Dotenv getConfig() {
+        return config;
+    }
+
+    // Setting up the Shard Manager getters
+    public ShardManager getShardManager() {
+        return shardManager;
+    }
+
+    public static void main(String[] args) {
+        try{
+            Main bot = new Main();
+        } catch (LoginException e){
+            System.out.println("ERROR: Provided Bot Token is Invalid!");
+        }
+    }
+}
---
+++ src/listeners/EventListeners.java
@@ -0,0 +1,78 @@
+package listeners;
+
+import net.dv8tion.jda.api.OnlineStatus;
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.User;
+import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
+import net.dv8tion.jda.api.events.guild.member.GuildMemberJoinEvent;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.events.message.react.MessageReactionAddEvent;
+import net.dv8tion.jda.api.events.user.update.UserUpdateOnlineStatusEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+
+public class EventListeners extends ListenerAdapter {
+    @Override
+    public void onMessageReactionAdd(MessageReactionAddEvent event) {
+        // If reaction is added, send a message to the server
+        // Example: User reacted to a message with a "{emoji}" in the #{chann} channel
+        User user = event.getUser();
+        String emoji = event.getReaction().getEmoji().getAsReactionCode();
+        String channelMention = event.getChannel().getAsMention();
+        String jumpLink = event.getJumpUrl();
+        TextChannel textChannel = event.getGuild().getTextChannelsByName("general", true).get(0);
+
+        assert user != null;
+        String message = user.getAsTag() + " reacted to a message with " + emoji + " in the " + channelMention + " channel!";
+
+        // Send it back to the Channel
+        // Sending to specific channel by name
+        textChannel.sendMessage(message).queue();
+
+        // Sending to current channel of the event
+//        event.getGuildChannel().sendMessage(message).queue();
+//        event.getChannel().sendMessage(message).queue();
+    }
+
+    @Override
+    public void onMessageReceived(@NotNull MessageReceivedEvent event) {
+        String message = event.getMessage().getContentRaw();
+
+        if (message.contains("ping")) {
+            event.getChannel().sendMessage("pong").queue();
+        }
+    }
+
+    @Override
+    public void onGuildMemberJoin(GuildMemberJoinEvent event) {
+        User user = event.getUser();
+        String avatar = event.getUser().getEffectiveAvatarUrl();
+        TextChannel textChannel = event.getGuild().getTextChannelsByName("welcome", true).get(0);
+
+        // Messages
+        String message = String.format("Welcome to the [Server] %s:  %s", event.getGuild(), user);
+
+        // Send the message
+        textChannel.sendMessage(message).queue();
+    }
+
+    @Override
+    public void onUserUpdateOnlineStatus(@NotNull UserUpdateOnlineStatusEvent event) {
+        List<Member> members = event.getGuild().getMembers();
+        int onlineMembers = 0;
+
+        for (Member member : members){
+            if (member.getOnlineStatus() == OnlineStatus.ONLINE){
+                onlineMembers++;
+            }
+        }
+
+        User user = event.getUser();
+        TextChannel textChannel = event.getGuild().getTextChannelsByName("general", true).get(0);
+        String message = "**" + user.getAsTag() + "** updated their online status! There are now " + onlineMembers + " users online on the server!";
+        textChannel.sendMessage(message).queue();
+//        Objects.requireNonNull(event.getGuild().getDefaultChannel()).asTextChannel().sendMessage(message).queue();
+    }
+}
